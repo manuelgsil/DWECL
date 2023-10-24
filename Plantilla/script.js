ce8a38bdaa940cec0435e23fc55c3b7d05970165
Manuel <guillenmanuel993@gmail.com>
2023-10-24 14:30:45 +0200

    update

---
+++ Plantilla/script.js
@@ -0,0 +1,8 @@
+function contador() {
+    console.log("🚀 ~ file: script.js:2 ~ contador ~ contador:", contador)
+    for (let index = 0; index < 50; index++) {
+        console.log(index);
+        
+    }
+    
+}
